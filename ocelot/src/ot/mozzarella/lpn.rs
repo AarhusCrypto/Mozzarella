98ed640e321472a4660701d4bf406656c3f3d2b8
Lennart Braun <lenerd@posteo.de>
2021-12-20 18:32:00 +0100

    cleanup mul/mul_add

--- ocelot/src/ot/mozzarella/lpn.rs
+++ ocelot/src/ot/mozzarella/lpn.rs
@@ -1,15 +1,13 @@
 use rand::{CryptoRng, Rng, SeedableRng};
 
-use scuttlebutt::{AesRng, Block};
-use scuttlebutt::ring::R64;
 use crate::ot::mozzarella::utils::gen_column;
-
+use rayon::prelude::*;
+use scuttlebutt::{ring::R64, AesRng, Block};
 
 // Z64 Local Linear Code with parameter D
 pub struct LLCode<const ROWS: usize, const COLS: usize, const D: usize> {
     indices: Vec<[(usize, R64); D]>,
 }
-
 
 // columns have length of rows
 impl<const ROWS: usize, const COLS: usize, const D: usize> LLCode<ROWS, COLS, D> {
@@ -34,36 +32,25 @@ impl<const ROWS: usize, const COLS: usize, const D: usize> LLCode<ROWS, COLS, D>
 
     // TODO: Can likely be made more efficient somehow?
     pub fn mul(&self, v: &[R64]) -> Vec<R64> {
-        let mut r: Vec<R64> = Vec::with_capacity(COLS);
-        for col in self.indices.iter() {
+        (self.indices.par_iter().map(|col| {
             let mut cord: R64 = R64::default();
-            let mut tmp: R64;
             for i in col.iter().copied() {
-                tmp = i.1;
-                tmp *= v[i.0];
-                cord += tmp;
+                cord += i.1 * v[i.0];
             }
-            r.push(cord);
-        }
-        r
+            cord
+        }))
+        .collect()
     }
 
     // takes the indices of the code (A) and adds them to elements of a.
     pub fn mul_add(&self, v: &[R64], a: &[R64]) -> Vec<R64> {
-        let mut out: Vec<R64> = Vec::new();
-        for (j, col) in self.indices.iter().enumerate() {
-            let mut tmp: R64;
+        (self.indices.par_iter().enumerate().map(|(j, col)| {
             let mut cord: R64 = R64::default();
             for i in col.iter().copied() {
-                tmp = i.1;
-                tmp *= v[i.0];
-                cord += tmp;
+                cord += i.1 * v[i.0];
             }
-            cord += a[j];
-            out.push(cord);
-        }
-        out
+            cord + a[j]
+        }))
+        .collect()
     }
 }
-
-
