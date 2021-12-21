bbc11c1e0810cf7eb412170aff1d58fccf31f232
Lennart Braun <lenerd@posteo.de>
2021-12-21 21:17:46 +0100

    Cleanup sp vole

--- ocelot/src/ot/mozzarella/spvole/prover.rs
+++ ocelot/src/ot/mozzarella/spvole/prover.rs
@@ -1,27 +1,31 @@
-use std::collections::HashSet;
-use std::iter::Sum;
-use std::time::Instant;
+use crate::{
+    ot::{
+        mozzarella::{cache::prover::CachedProver, ggm::prover as ggmProver, utils::unpack_bits},
+        CorrelatedReceiver,
+        RandomReceiver,
+        Receiver as OtReceiver,
+    },
+    Error,
+};
 use rand::{CryptoRng, Rng, SeedableRng};
-use scuttlebutt::{AbstractChannel, AesRng, Block};
-use scuttlebutt::ring::R64;
-use crate::Error;
-use crate::ot::mozzarella::ggm::prover as ggmProver;
-use crate::ot::{CorrelatedReceiver, RandomReceiver, Receiver as OtReceiver};
-use crate::ot::mozzarella::cache::prover::CachedProver;
-use crate::ot::mozzarella::utils::unpack_bits;
+use scuttlebutt::{ring::R64, AbstractChannel, AesRng, Block};
+use std::time::Instant;
 
 pub struct Prover {}
 
 impl Prover {
-
     pub fn init() -> Self {
-        Self{}
+        Self {}
     }
 
     #[allow(non_snake_case)]
     pub fn extend<
         OT: OtReceiver<Msg = Block> + CorrelatedReceiver + RandomReceiver,
-        C: AbstractChannel, RNG: CryptoRng + Rng, const N: usize, const H: usize>(
+        C: AbstractChannel,
+        RNG: CryptoRng + Rng,
+        const N: usize,
+        const H: usize,
+    >(
         &mut self,
         channel: &mut C,
         rng: &mut RNG,
@@ -30,7 +34,6 @@ impl Prover {
         cache: &mut CachedProver,
         alphas: &[usize],
     ) -> Result<(Vec<[R64; N]>, Vec<[R64; N]>), Error> {
-
         // Spawning these with_capicity requires too much space
         let mut out_w: Vec<[R64; N]> = Vec::new();
         let mut out_u: Vec<[R64; N]> = Vec::new();
@@ -41,87 +44,76 @@ impl Prover {
 
             let path: [bool; H] = unpack_bits::<H>(alpha);
 
-
-            let mut w: [R64;N] = [R64::default(); N];
             let (a, c): (R64, R64) = cache.pop();
             let delta: R64 = c;
-            let mut beta: R64 = R64(rng.next_u64());
-            loop {
-                if beta.0 != 0 {
-                    break;
+            let beta: R64 = {
+                let mut beta = R64(rng.next_u64());
+                while beta.0 == 0 {
+                    beta = R64(rng.next_u64());
                 }
-                beta = R64(rng.next_u64());
-            }
-            let mut a_prime = beta;
-            a_prime -= a;
+                beta
+            };
+            let a_prime = beta - a;
             channel.send(&a_prime).unwrap();
 
             let mut ggm_prover = ggmProver::Prover::init();
             let start = Instant::now();
-            let (v, path_index) = ggm_prover.gen_eval(channel, ot_receiver, &path)?;
+            let (v, _) = ggm_prover.gen_eval(channel, ot_receiver, &path)?;
             println!("PROVER_GGM_EVAL:\t {:?}", start.elapsed());
-
 
             let d: R64 = channel.receive()?;
 
-            let mut w_alpha: R64 = delta;
-            w_alpha -= d;
-            w_alpha -= R64::sum(v.to_vec().into_iter()); // disgusting; can this be fixed to not require a vector?
-            w = v;
-            w[path_index.clone()] = w_alpha;
+            let w_alpha: R64 = delta - d - v.iter().sum();
+            let mut w: [R64; N] = v;
+            w[alpha] = w_alpha;
 
-
-            //let mut indices = HashSet::new();
-            let mut indices = [false; N];
-
+            // sample bit vector chi with Hamming weight N/2
 
             let seed: Block = rng.gen();
-            let mut new_rng = AesRng::from_seed(seed);
-
-
-            // TODO: approximate rather than strictly require N/2
-            // N will always be even
-            let mut i = 0;
-            while i < N/2 {
-                let tmp: usize = new_rng.gen_range(0, N);
-                if indices[tmp] {
-                    continue
-                }
-                indices[tmp] = true;
-                i += 1;
-            }
-
             channel.send(&seed).unwrap();
 
+            let indices: [bool; N] = {
+                let mut indices = [false; N];
+                let mut new_rng = AesRng::from_seed(seed);
 
-            let copied_indices = indices.clone();
-            let tmp = path_index.clone();
-            let chi_alpha: R64 = R64(if copied_indices[tmp] { 1 } else { 0 });
-            let (x,z): (R64, R64) = cache.pop();
+                // TODO: approximate rather than strictly require N/2
+                // N will always be even
+                let mut i = 0;
+                while i < N / 2 {
+                    let tmp: usize = new_rng.gen_range(0, N);
+                    if indices[tmp] {
+                        continue;
+                    }
+                    indices[tmp] = true;
+                    i += 1;
+                }
+                indices
+            };
 
-            let mut x_star: R64 = beta;
-            x_star *= chi_alpha;
-            x_star -= x;
+            let chi_alpha: R64 = R64(if indices[alpha] { 1 } else { 0 });
+            let (x, z): (R64, R64) = cache.pop();
+
+            let x_star: R64 = chi_alpha * beta - x;
 
             channel.send(&x_star).unwrap();
 
-
             // TODO: apparently map is quite slow on large arrays -- is our use-case "large"?
-            let tmp_sum = indices.iter().zip(w).filter(|x| *x.0).map(|x| x.1);
+            let VP: R64 = indices
+                .iter()
+                .zip(&w)
+                .filter(|x| *x.0)
+                .map(|x| x.1)
+                .sum::<R64>()
+                - z;
 
-
-
-            let mut VP = R64::sum(tmp_sum.into_iter());
-            VP -= z;
-
+            // TODO: implement F_EQ functionality
             channel.send(&VP).unwrap();
 
             let mut u: [R64; N] = [R64::default(); N];
-            u[path_index] = beta;
+            u[alpha] = beta;
 
             out_w.push(w);
             out_u.push(u);
-
         }
 
         return Ok((out_w, out_u));
--- ocelot/src/ot/mozzarella/spvole/verifier.rs
+++ ocelot/src/ot/mozzarella/spvole/verifier.rs
@@ -1,15 +1,18 @@
-use std::collections::HashSet;
-use std::iter::Sum;
-use std::time::Instant;
-use crate::errors::Error;
+use crate::{
+    errors::Error,
+    ot::{
+        mozzarella::ggm::verifier as ggmVerifier,
+        CorrelatedSender,
+        RandomSender,
+        Sender as OtSender,
+    },
+};
 use rand::{CryptoRng, Rng, SeedableRng};
 use scuttlebutt::{AbstractChannel, AesRng, Block};
-use crate::ot::mozzarella::ggm::verifier as ggmVerifier;
-use crate::ot::{Sender as OtSender, RandomSender, CorrelatedSender};
+use std::time::Instant;
 
-
-use scuttlebutt::ring::R64;
 use crate::ot::mozzarella::cache::verifier::CachedVerifier;
+use scuttlebutt::ring::R64;
 
 pub struct Verifier {
     pub delta: R64, // tmp
@@ -17,14 +20,13 @@ pub struct Verifier {
 
 impl Verifier {
     pub fn init(delta: R64) -> Self {
-        Self {
-            delta,
-        }
+        Self { delta }
     }
     #[allow(non_snake_case)]
     pub fn extend<
         OT: OtSender<Msg = Block> + CorrelatedSender + RandomSender,
-        C: AbstractChannel, RNG: CryptoRng + Rng,
+        C: AbstractChannel,
+        RNG: CryptoRng + Rng,
         const N: usize,
         const H: usize,
     >(
@@ -34,11 +36,11 @@ impl Verifier {
         num: usize, // number of repetitions
         ot_sender: &mut OT,
         cache: &mut CachedVerifier,
-    ) ->Result<Vec<[R64;N]>, Error> {
+    ) -> Result<Vec<[R64; N]>, Error> {
         assert_eq!(1 << H, N);
 
         // create result vector
-        let mut vs: Vec<[R64;N]> = Vec::with_capacity(num); // make stuff array as quicker
+        let mut vs: Vec<[R64; N]> = Vec::with_capacity(num); // make stuff array as quicker
         unsafe { vs.set_len(num) };
 
         // generate the trees before, as we must now use OT to deliver the keys
@@ -47,11 +49,7 @@ impl Verifier {
             let b: R64 = cache.pop(); // some kind of error handling in case this cannot be done
 
             let a_prime: R64 = channel.receive()?;
-            let mut gamma = b;
-            let mut tmp = self.delta;
-            tmp *= a_prime;
-            gamma -= tmp;
-
+            let gamma = b - self.delta * a_prime;
 
             let mut m: [(Block, Block); H] = [(Default::default(), Default::default()); H];
 
@@ -62,48 +60,48 @@ impl Verifier {
             let s: [Block; N] = ggm_verifier.gen_tree(channel, ot_sender, &mut m)?;
             println!("VERIFIER_GGM_INIT:\t {:?}", start.elapsed());
 
-            let ggm_out:[R64;N] = s.map(|x| R64::from(x.extract_0_u64()));
+            let ggm_out: [R64; N] = s.map(|x| R64::from(x.extract_0_u64()));
 
             // compute d = gamma - \sum_{i \in [n]} v[i]
-            let mut d: R64 = gamma;
-
-
-            d -= R64::sum(ggm_out.to_vec().into_iter()); // this sucks
+            let d: R64 = gamma - ggm_out.iter().sum();
 
             channel.send(&d).unwrap();
 
             let y_star: R64 = cache.pop();
 
-
-            // TODO: optimise to be "roughly" N/2
-            let mut indices = [false; N];
             let seed: Block = channel.receive().unwrap();
-            let mut new_rng = AesRng::from_seed(seed);
+            // expand seed into bit vector chi
+            // TODO: optimise to be "roughly" N/2
+            let indices: [bool; N] = {
+                let mut indices = [false; N];
+                let mut new_rng = AesRng::from_seed(seed);
 
-            // N will always be even
-            let mut i = 0;
-            while i < N/2 {
-                let tmp: usize = new_rng.gen_range(0, N);
-                if indices[tmp] {
-                    continue
+                // N will always be even
+                let mut i = 0;
+                while i < N / 2 {
+                    let tmp: usize = new_rng.gen_range(0, N);
+                    if indices[tmp] {
+                        continue;
+                    }
+                    indices[tmp] = true;
+                    i += 1;
                 }
-                indices[tmp] = true;
-                i += 1;
-            }
+                indices
+            };
 
             let x_star: R64 = channel.receive()?;
-            let mut y: R64 = y_star;
-            let mut tmp = self.delta;
-            tmp *= x_star;
-            y -= tmp;
+            let y: R64 = y_star - self.delta * x_star;
 
-            let tmp_sum = indices.iter().zip(ggm_out).filter(|x| *x.0).map(|x| x.1);
+            let VV: R64 = indices
+                .iter()
+                .zip(&ggm_out)
+                .filter(|x| *x.0)
+                .map(|x| x.1)
+                .sum::<R64>()
+                - y;
 
-            let mut VV = R64::sum(tmp_sum.into_iter());
-            VV -= y;
-
+            // TODO: implement F_EQ functionality
             let VP = channel.receive()?;
-
             assert_eq!(VV, VP);
 
             vs[rep] = ggm_out;
