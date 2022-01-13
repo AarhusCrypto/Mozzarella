884582ff834e31321e4898648c1559e36f8d857f
Lennart Braun <lenerd@posteo.de>
2022-01-13 18:36:20 +0100

    parallelize batched ggm gen/eval/check

--- ocelot/src/ot/mozzarella/ggm/prover.rs
+++ ocelot/src/ot/mozzarella/ggm/prover.rs
@@ -7,7 +7,8 @@ use crate::{
         Receiver as OtReceiver,
     },
 };
-use rand::{distributions::Uniform, Rng};
+use rand::Rng;
+use rayon::prelude::*;
 use scuttlebutt::{AbstractChannel, AesHash, AesRng, Block, F128};
 
 use crate::ot::mozzarella::utils::prg2;
@@ -246,7 +247,7 @@ pub struct BatchedProver {
     num_instances: usize,
     tree_height: usize,
     output_size: usize,
-    hash: AesHash,
+    _hash: AesHash,
     rng: AesRng,
     alpha_bits_s: Vec<bool>,
     layer_keys_s: Vec<Block>,
@@ -264,7 +265,7 @@ impl BatchedProver {
             num_instances,
             tree_height,
             output_size,
-            hash: AesHash::new(Default::default()),
+            _hash: AesHash::new(Default::default()),
             rng: AesRng::new(),
             alpha_bits_s: vec![false; num_instances * tree_height],
             layer_keys_s: vec![Default::default(); num_instances * tree_height],
@@ -320,99 +321,51 @@ impl BatchedProver {
         Ok(())
     }
 
-    pub fn eval(&mut self) {
-        for tree_i in 0..self.num_instances {
-            let tree_output_size_offset = tree_i * self.output_size;
-            let tree_height_offset = tree_i * self.tree_height;
+    pub fn eval_helper(
+        output_size: usize,
+        tree_height: usize,
+        hash: &AesHash,
+        alpha_bits: &[bool],
+        final_layer_blocks: &mut [Block],
+        final_layer_check_values: &mut [F128],
+        layer_keys: &[Block],
+        final_key: &Block,
+    ) {
+        assert_eq!(alpha_bits.len(), tree_height);
+        assert_eq!(final_layer_blocks.len(), output_size);
+        assert_eq!(final_layer_check_values.len(), output_size);
+        assert_eq!(layer_keys.len(), tree_height);
 
-            let mut out = vec![Block::default(); self.output_size]; // TODO: reuse self.final_layer_blocks
+        let mut out = vec![Block::default(); output_size]; // TODO: reuse self.final_layer_blocks
 
-            // idea: iteratively treat each key as a root and compute its leafs -- this requires storing a matrix though
-            // protocol
-            // Start with a single key (either 0 or 1), compute next layer using this
-            // Just try to compute as many values as possible. Perhaps fill out the set beforehand with values we know for stuff to not compute?
-            // or just check whenever we set the
+        // idea: iteratively treat each key as a root and compute its leafs -- this requires storing a matrix though
+        // protocol
+        // Start with a single key (either 0 or 1), compute next layer using this
+        // Just try to compute as many values as possible. Perhaps fill out the set beforehand with values we know for stuff to not compute?
+        // or just check whenever we set the
 
-            // for each layer, note if we are to compute even or odd perhaps?
+        // for each layer, note if we are to compute even or odd perhaps?
 
-            // path is easily computable: pack the bits again and use the result as an index
-            // compute keyed index using this path: if 1 - alpha[i] = 0 : key = path - 1 else key = path + 1
+        // path is easily computable: pack the bits again and use the result as an index
+        // compute keyed index using this path: if 1 - alpha[i] = 0 : key = path - 1 else key = path + 1
 
-            let mut path_index: usize = 0;
-            let mut keyed_index;
+        let mut path_index: usize = 0;
+        let mut keyed_index;
 
-            // keep track of the current path index as well as keyed index -- can likely be optimised to avoid the two shifts
-            let index = if self.alpha_bits_s[tree_height_offset + 0] {
-                1
-            } else {
-                0
-            };
-            path_index += index;
-            keyed_index = if 1 - index == 0 {
-                path_index - 1
-            } else {
-                path_index + 1
-            };
+        // keep track of the current path index as well as keyed index -- can likely be optimised to avoid the two shifts
+        let index = if alpha_bits[0] { 1 } else { 0 };
+        path_index += index;
+        keyed_index = if 1 - index == 0 {
+            path_index - 1
+        } else {
+            path_index + 1
+        };
 
-            out[keyed_index] = self.layer_keys_s[tree_height_offset + 0]; // set initial key
-                                                                          //println!("INFO:\tComputing Keyed Index ({}): {}", keyed_index, out[keyed_index]);
-            for i in 1..self.tree_height {
-                let mut m = Block::default();
-                let mut j = (1 << i) - 1;
-                loop {
-                    if j == path_index {
-                        if j == 0 {
-                            break;
-                        }
-                        j -= 1;
-                        continue;
-                    }
-
-                    let (s0, s1) = prg2(&self.hash, out[j]);
-                    if !self.alpha_bits_s[tree_height_offset + i] {
-                        m ^= s1; // keep track of the complete XORs of each layer
-                    } else {
-                        m ^= s0; // keep track of the complete XORs of each layer
-                    }
-                    //println!("DEBUG:\tValue of m ({}): {}", if alphas[0] { 2 * j } else { 2 * j + 1 }, m[0]);
-
-                    out[2 * j] = s0;
-                    out[2 * j + 1] = s1;
-
-                    if j == 0 {
-                        break;
-                    }
-                    j -= 1;
-                }
-
-                let index = if self.alpha_bits_s[tree_height_offset + i] {
-                    1
-                } else {
-                    0
-                };
-                path_index = 0;
-                for tmp in 0..i + 1 {
-                    let alpha_tmp = if self.alpha_bits_s[tree_height_offset + i - tmp] {
-                        1
-                    } else {
-                        0
-                    };
-                    path_index += alpha_tmp * (1 << (tmp));
-                }
-                keyed_index = if 1 - index == 0 {
-                    path_index - 1
-                } else {
-                    path_index + 1
-                };
-
-                //println!("DEBUG:\tXORing {} ^ {}", K[i], m[i - 1]);
-                out[keyed_index] = self.layer_keys_s[tree_height_offset + i] ^ m;
-                //println!("INFO:\tComputing Keyed Index ({}): {}", keyed_index, out[keyed_index]);
-            }
-
-            // compute final layer
-            let mut j = (1 << self.tree_height) - 1;
-            let mut last_layer_key = Block::default();
+        out[keyed_index] = layer_keys[0]; // set initial key
+                                          //println!("INFO:\tComputing Keyed Index ({}): {}", keyed_index, out[keyed_index]);
+        for i in 1..tree_height {
+            let mut m = Block::default();
+            let mut j = (1 << i) - 1;
             loop {
                 if j == path_index {
                     if j == 0 {
@@ -422,11 +375,16 @@ impl BatchedProver {
                     continue;
                 }
 
-                let (s0, s1) = prg2(&self.hash, out[j]);
-                last_layer_key ^= s1;
+                let (s0, s1) = prg2(hash, out[j]);
+                if !alpha_bits[i] {
+                    m ^= s1; // keep track of the complete XORs of each layer
+                } else {
+                    m ^= s0; // keep track of the complete XORs of each layer
+                }
+                //println!("DEBUG:\tValue of m ({}): {}", if alphas[0] { 2 * j } else { 2 * j + 1 }, m[0]);
 
-                self.final_layer_blocks_s[tree_output_size_offset + j] = s0;
-                self.final_layer_check_values_s[tree_output_size_offset + j] = F128::from(s1);
+                out[2 * j] = s0;
+                out[2 * j + 1] = s1;
 
                 if j == 0 {
                     break;
@@ -434,9 +392,84 @@ impl BatchedProver {
                 j -= 1;
             }
 
-            self.final_layer_check_values_s[tree_output_size_offset + path_index] =
-                F128::from(last_layer_key ^ self.final_key_s[tree_i]);
+            let index = if alpha_bits[i] { 1 } else { 0 };
+            path_index = 0;
+            for tmp in 0..i + 1 {
+                let alpha_tmp = if alpha_bits[i - tmp] { 1 } else { 0 };
+                path_index += alpha_tmp * (1 << (tmp));
+            }
+            keyed_index = if 1 - index == 0 {
+                path_index - 1
+            } else {
+                path_index + 1
+            };
+
+            //println!("DEBUG:\tXORing {} ^ {}", K[i], m[i - 1]);
+            out[keyed_index] = layer_keys[i] ^ m;
+            //println!("INFO:\tComputing Keyed Index ({}): {}", keyed_index, out[keyed_index]);
         }
+
+        // compute final layer
+        let mut j = (1 << tree_height) - 1;
+        let mut last_layer_key = Block::default();
+        loop {
+            if j == path_index {
+                if j == 0 {
+                    break;
+                }
+                j -= 1;
+                continue;
+            }
+
+            let (s0, s1) = prg2(hash, out[j]);
+            last_layer_key ^= s1;
+
+            final_layer_blocks[j] = s0;
+            final_layer_check_values[j] = F128::from(s1);
+
+            if j == 0 {
+                break;
+            }
+            j -= 1;
+        }
+
+        final_layer_check_values[path_index] = F128::from(last_layer_key ^ *final_key);
+    }
+
+    pub fn eval(&mut self) {
+        let output_size = self.output_size;
+        let tree_height = self.tree_height;
+        let hash = AesHash::new(Default::default()); // TODO: improve this
+        (
+            self.alpha_bits_s.par_chunks_exact_mut(self.tree_height),
+            self.final_layer_blocks_s
+                .par_chunks_exact_mut(self.output_size),
+            self.final_layer_check_values_s
+                .par_chunks_exact_mut(self.output_size),
+            self.layer_keys_s.par_chunks_exact(self.tree_height),
+            self.final_key_s.par_iter(),
+        )
+            .into_par_iter()
+            .for_each(
+                |(
+                    alpha_bits,
+                    final_layer_blocks,
+                    final_layer_check_values,
+                    layer_keys,
+                    final_key,
+                )| {
+                    Self::eval_helper(
+                        output_size,
+                        tree_height,
+                        &hash,
+                        alpha_bits,
+                        final_layer_blocks,
+                        final_layer_check_values,
+                        layer_keys,
+                        final_key,
+                    );
+                },
+            );
     }
 
     pub fn send_challenge<C: AbstractChannel>(&mut self, channel: &mut C) -> Result<(), Error> {
@@ -448,26 +481,34 @@ impl BatchedProver {
         Ok(())
     }
 
-    pub fn compute_hash(&mut self) {
-        for tree_i in 0..self.num_instances {
-            let mut gen = BiasedGen::new(self.challenge_seed_s[tree_i]);
-            /*
-            TODO: Can we do this for all the ggm trees at once, so we gen n GGM trees
-               and then check them all by the end of the protocol?
-            */
-            // THIS CODE COMPUTES \sum_ i \in [n] xi_i * c_i
-            let mut capital_gamma = (Block::default(), Block::default()); // defer GF(2^128) reduction
-            for cv in self.final_layer_check_values_s
-                [tree_i * self.output_size..(tree_i + 1) * self.output_size]
-                .iter()
-            {
-                let xli: F128 = gen.next();
-                let cm = xli.cmul(*cv);
-                capital_gamma.0 ^= cm.0;
-                capital_gamma.1 ^= cm.1;
-            }
-            self.challenge_hash_s[tree_i] = F128::reduce(capital_gamma);
+    fn compute_hash_helper(challenge_seed: &Block, final_layer_check_values: &[F128]) -> F128 {
+        let mut gen = BiasedGen::new(*challenge_seed);
+        /*
+        TODO: Can we do this for all the ggm trees at once, so we gen n GGM trees
+           and then check them all by the end of the protocol?
+        */
+        // THIS CODE COMPUTES \sum_ i \in [n] xi_i * c_i
+        let mut capital_gamma = (Block::default(), Block::default()); // defer GF(2^128) reduction
+        for &cv in final_layer_check_values {
+            let xli: F128 = gen.next();
+            let cm = xli.cmul(cv);
+            capital_gamma.0 ^= cm.0;
+            capital_gamma.1 ^= cm.1;
         }
+        F128::reduce(capital_gamma)
+    }
+
+    pub fn compute_hash(&mut self) {
+        (
+            self.challenge_seed_s.par_iter(),
+            self.final_layer_check_values_s
+                .par_chunks_exact(self.output_size),
+            self.challenge_hash_s.par_iter_mut(),
+        )
+            .into_par_iter()
+            .for_each(|(challenge_seed, final_layer_keys, challenge_hash)| {
+                *challenge_hash = Self::compute_hash_helper(challenge_seed, final_layer_keys);
+            });
     }
 
     pub fn receive_response_and_check<C: AbstractChannel>(&self, channel: &mut C) -> bool {
--- ocelot/src/ot/mozzarella/ggm/verifier.rs
+++ ocelot/src/ot/mozzarella/ggm/verifier.rs
@@ -8,6 +8,7 @@ use crate::{
     },
 };
 use rand::Rng;
+use rayon::prelude::*;
 use scuttlebutt::{AbstractChannel, AesHash, AesRng, Block, F128};
 
 use crate::ot::mozzarella::utils;
@@ -167,10 +168,10 @@ pub struct BatchedVerifier {
     num_instances: usize,
     tree_height: usize,
     output_size: usize,
-    hash: AesHash,
+    _hash: AesHash,
     rng: AesRng,
     layer_key_pairs_s: Vec<(Block, Block)>,
-    final_layer_keys_s: Vec<Block>,
+    final_layer_check_values_s: Vec<F128>,
     final_layer_blocks_s: Vec<Block>,
     final_key_s: Vec<Block>,
     challenge_seed_s: Vec<Block>,
@@ -185,10 +186,10 @@ impl BatchedVerifier {
             num_instances,
             tree_height,
             output_size,
-            hash: AesHash::new(Default::default()),
+            _hash: AesHash::new(Default::default()),
             rng: AesRng::new(),
             layer_key_pairs_s: vec![Default::default(); num_instances * tree_height],
-            final_layer_keys_s: vec![Default::default(); num_instances * output_size],
+            final_layer_check_values_s: vec![Default::default(); num_instances * output_size],
             final_layer_blocks_s: vec![Default::default(); num_instances * output_size],
             final_key_s: vec![Default::default(); num_instances],
             challenge_seed_s: vec![Default::default(); num_instances],
@@ -200,61 +201,93 @@ impl BatchedVerifier {
         self.final_layer_blocks_s.as_slice()
     }
 
-    pub fn gen(&mut self) {
-        for tree_i in 0..self.num_instances {
-            let tree_output_size_offset = tree_i * self.output_size;
-            let tree_height_offset = tree_i * self.tree_height;
-            self.final_layer_blocks_s[tree_output_size_offset + 0] = self.rng.gen();
+    fn gen_helper(
+        output_size: usize,
+        tree_height: usize,
+        hash: &AesHash,
+        final_layer_blocks: &mut [Block],
+        final_layer_check_values: &mut [F128],
+        layer_key_pairs: &mut [(Block, Block)],
+        final_key: &mut Block,
+    ) {
+        assert_eq!(final_layer_blocks.len(), output_size);
+        assert_eq!(final_layer_check_values.len(), output_size);
+        assert_eq!(layer_key_pairs.len(), tree_height);
 
-            /*
-               STEPS:
-               1) Compute length-doubling prg for each node until the last layer
-               2) Compute "final_prg" for the last layer, resulting in 2*N elements
-            */
+        /*
+           STEPS:
+           1) Compute length-doubling prg for each node until the last layer
+           2) Compute "final_prg" for the last layer, resulting in 2*N elements
+        */
 
-            // for the final layer we need to treat the elements as field elements, but this is done by
-            // simply taking mod 2^k I guess of the additions. Currently this things loops all the way
-            // to H, but we should stop earlier if we do not do the final step to make sure it's also secure!
-            // this assumes it's secure.. Steps d-f is missing to compute the values that would be used to verify
-            for i in 0..self.tree_height {
-                let mut j = (1 << i) - 1;
-                loop {
-                    let res = utils::prg2(
-                        &self.hash,
-                        self.final_layer_blocks_s[tree_output_size_offset + j],
-                    );
-                    self.layer_key_pairs_s[tree_height_offset + i].0 ^= res.0; // keep track of the complete XORs of each layer
-                    self.layer_key_pairs_s[tree_height_offset + i].1 ^= res.1; // keep track of the complete XORs of each layer
-
-                    self.final_layer_blocks_s[tree_output_size_offset + 2 * j] = res.0;
-                    //println!("INFO:\ti:{}\tWriting to {}", i, s[2*j]);
-                    self.final_layer_blocks_s[tree_output_size_offset + 2 * j + 1] = res.1;
-                    //println!("INFO:\ti:{}\tWriting to {}", i, s[2*j+1]);
-                    if j == 0 {
-                        break;
-                    }
-                    j -= 1;
-                }
-            }
-
-            let mut final_key = Block::default();
-            // compute the final layer
-            let mut j = (1 << self.tree_height) - 1;
+        // for the final layer we need to treat the elements as field elements, but this is done by
+        // simply taking mod 2^k I guess of the additions. Currently this things loops all the way
+        // to H, but we should stop earlier if we do not do the final step to make sure it's also secure!
+        // this assumes it's secure.. Steps d-f is missing to compute the values that would be used to verify
+        for i in 0..tree_height {
+            let mut j = (1 << i) - 1;
             loop {
-                let res = utils::prg2(
-                    &self.hash,
-                    self.final_layer_blocks_s[tree_output_size_offset + j],
-                );
-                final_key ^= res.1; // keep track of the complete XORs of each layer
-                self.final_layer_blocks_s[tree_output_size_offset + j] = res.0;
-                self.final_layer_keys_s[tree_output_size_offset + j] = res.1;
+                let res = utils::prg2(hash, final_layer_blocks[j]);
+                layer_key_pairs[i].0 ^= res.0; // keep track of the complete XORs of each layer
+                layer_key_pairs[i].1 ^= res.1; // keep track of the complete XORs of each layer
+
+                final_layer_blocks[2 * j] = res.0;
+                //println!("INFO:\ti:{}\tWriting to {}", i, s[2*j]);
+                final_layer_blocks[2 * j + 1] = res.1;
+                //println!("INFO:\ti:{}\tWriting to {}", i, s[2*j+1]);
                 if j == 0 {
                     break;
                 }
                 j -= 1;
             }
-            self.final_key_s[tree_i] = final_key;
         }
+
+        *final_key = Block::default();
+        // compute the final layer
+        let mut j = (1 << tree_height) - 1;
+        loop {
+            let res = utils::prg2(hash, final_layer_blocks[j]);
+            *final_key ^= res.1; // keep track of the complete XORs of each layer
+            final_layer_blocks[j] = res.0;
+            final_layer_check_values[j] = res.1.into();
+            if j == 0 {
+                break;
+            }
+            j -= 1;
+        }
+    }
+
+    pub fn gen(&mut self) {
+        for tree_i in 0..self.num_instances {
+            // sample seeds
+            self.final_layer_blocks_s[tree_i * self.output_size] = self.rng.gen();
+        }
+        let output_size = self.output_size;
+        let tree_height = self.tree_height;
+        let hash = AesHash::new(Default::default()); // TODO: improve this
+        (
+            self.final_layer_blocks_s
+                .par_chunks_exact_mut(self.output_size),
+            self.final_layer_check_values_s
+                .par_chunks_exact_mut(self.output_size),
+            self.layer_key_pairs_s
+                .par_chunks_exact_mut(self.tree_height),
+            self.final_key_s.par_iter_mut(),
+        )
+            .into_par_iter()
+            .for_each(
+                |(final_layer_blocks, final_layer_keys, layer_key_pairs, final_key)| {
+                    Self::gen_helper(
+                        output_size,
+                        tree_height,
+                        &hash,
+                        final_layer_blocks,
+                        final_layer_keys,
+                        layer_key_pairs,
+                        final_key,
+                    );
+                },
+            );
     }
 
     pub fn send_layer_keys<
@@ -293,19 +326,33 @@ impl BatchedVerifier {
         Ok(())
     }
 
-    pub fn compute_response(&mut self) {
-        for tree_i in 0..self.num_instances {
-            let tree_output_size_offset = tree_i * self.output_size;
-            let mut gen = BiasedGen::new(self.challenge_seed_s[tree_i]);
-            let mut capital_gamma = (Block::default(), Block::default());
-            for idx in 0..self.output_size {
-                let xli: F128 = gen.next();
-                let cm = xli.cmul(self.final_layer_keys_s[tree_output_size_offset + idx].into());
-                capital_gamma.0 ^= cm.0;
-                capital_gamma.1 ^= cm.1;
-            }
-            self.challenge_response_s[tree_i] = F128::reduce(capital_gamma);
+    pub fn compute_response_helper(
+        challenge_seed: &Block,
+        final_layer_check_values: &[F128],
+    ) -> F128 {
+        let mut gen = BiasedGen::new(*challenge_seed);
+        let mut capital_gamma = (Block::default(), Block::default());
+        for &cv in final_layer_check_values {
+            let xli: F128 = gen.next();
+            let cm = xli.cmul(cv);
+            capital_gamma.0 ^= cm.0;
+            capital_gamma.1 ^= cm.1;
         }
+        F128::reduce(capital_gamma)
+    }
+
+    pub fn compute_response(&mut self) {
+        (
+            self.challenge_seed_s.par_iter(),
+            self.final_layer_check_values_s
+                .par_chunks_exact(self.output_size),
+            self.challenge_response_s.par_iter_mut(),
+        )
+            .into_par_iter()
+            .for_each(|(challenge_seed, final_layer_keys, challenge_response)| {
+                *challenge_response =
+                    Self::compute_response_helper(challenge_seed, final_layer_keys);
+            });
     }
 
     pub fn send_response<C: AbstractChannel>(&mut self, channel: &mut C) -> Result<(), Error> {
