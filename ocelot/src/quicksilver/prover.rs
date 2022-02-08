a52638da172ddfd4371bb5dfbc1d81ce9494ae3e
Lennart Braun <lenerd@posteo.de>
2022-02-08 10:52:57 +0100

    cleanup quicksilver code

--- ocelot/src/quicksilver/prover.rs
+++ ocelot/src/quicksilver/prover.rs
@@ -1,25 +1,22 @@
-use std::sync::mpsc::channel;
-use std::time::{Duration, Instant};
-use rand::distributions::{Distribution, Standard};
-use rand::{Rng, SeedableRng};
-use rayon::iter::{IndexedParallelIterator, IntoParallelRefIterator, ParallelIterator};
-use rayon::prelude::{IntoParallelIterator, ParallelSliceMut};
-use scuttlebutt::{AbstractChannel, AesRng, Block};
-use scuttlebutt::channel::{Receivable, Sendable};
-use scuttlebutt::ring::NewRing;
-use crate::Error;
-use crate::ot::mozzarella::{MozzarellaProver, MozzarellaProverStats};
 use crate::ot::mozzarella::cache::prover::CachedProver;
 use crate::ot::mozzarella::lpn::LLCode;
+use crate::ot::mozzarella::{MozzarellaProver, MozzarellaProverStats};
+use crate::Error;
+use rand::distributions::{Distribution, Standard};
+use rand::{Rng, SeedableRng};
+use rayon::prelude::*;
+use scuttlebutt::channel::{Receivable, Sendable};
+use scuttlebutt::ring::NewRing;
+use scuttlebutt::{AbstractChannel, AesRng, Block};
 use serde::Serialize;
-
+use std::time::{Duration, Instant};
 
 #[allow(non_snake_case)]
 pub struct Prover<'a, RingT>
-    where
-        RingT: NewRing + Receivable,
-        Standard: Distribution<RingT>,
-        for<'b> &'b RingT: Sendable,
+where
+    RingT: NewRing + Receivable,
+    Standard: Distribution<RingT>,
+    for<'b> &'b RingT: Sendable,
 {
     mozProver: MozzarellaProver<'a, RingT>,
     stats: ProverStats,
@@ -32,23 +29,21 @@ pub struct ProverStats {
     pub mozzarella_stats: MozzarellaProverStats,
 }
 
-
 #[allow(non_snake_case)]
-impl <'a, RingT: NewRing> Prover<'a, RingT>
+impl<'a, RingT: NewRing> Prover<'a, RingT>
 where
     RingT: NewRing + Receivable,
     Standard: Distribution<RingT>,
     for<'b> &'b RingT: Sendable,
 {
     pub fn init<C: AbstractChannel>(
-                    code: &'a LLCode<RingT>,
-                    channel: &mut C,
-                    cache: CachedProver<RingT>,
-                    base_vole_len: usize,
-                    num_sp_voles: usize,
-                    sp_vole_len: usize,
+        code: &'a LLCode<RingT>,
+        channel: &mut C,
+        cache: CachedProver<RingT>,
+        base_vole_len: usize,
+        num_sp_voles: usize,
+        sp_vole_len: usize,
     ) -> Self {
-
         let mut mozProver = MozzarellaProver::<RingT>::new(
             cache,
             &code,
@@ -66,10 +61,7 @@ where
 
         // todo: Extend here for easier timing
 
-        Self {
-            mozProver,
-            stats
-        }
+        Self { mozProver, stats }
     }
 
     pub fn get_stats(&mut self) -> ProverStats {
@@ -82,62 +74,57 @@ where
     }
 
     // The mozVerifier already handles if there aren't any left, in which case it runs extend
-    pub fn random<C: AbstractChannel>(
-        &mut self,
-        channel: &mut C,
-    ) -> Result<(RingT, RingT), Error> {
-        let (x, z)  = self.mozProver.vole(channel)?;
-        return Ok((x,z))
+    pub fn random<C: AbstractChannel>(&mut self, channel: &mut C) -> Result<(RingT, RingT), Error> {
+        let (x, z) = self.mozProver.vole(channel)?;
+        return Ok((x, z));
     }
 
     pub fn input<C: AbstractChannel>(
         &mut self,
         channel: &mut C,
         x: RingT,
-    ) -> Result<(RingT, RingT), Error>{
+    ) -> Result<(RingT, RingT), Error> {
         //println!("I'm here");
-        let (r,z) = self.random(channel)?;
+        let (r, z) = self.random(channel)?;
         //println!("I'm here now");
         let y = x - r;
-        channel.send(&y);
+        channel.send(&y)?;
 
         Ok((x, z))
     }
 
-    pub fn add (
+    pub fn add(
         &mut self,
         (alpha, alpha_mac): (RingT, RingT),
         (beta, beta_mac): (RingT, RingT),
     ) -> Result<(RingT, RingT), Error> {
-
-        Ok(((alpha + beta), ( alpha_mac + beta_mac)))
+        Ok(((alpha + beta), (alpha_mac + beta_mac)))
     }
 
-    pub fn multiply<C: AbstractChannel> (
+    pub fn multiply<C: AbstractChannel>(
         &mut self,
         channel: &mut C,
         (alpha, alpha_mac): (RingT, RingT),
         (beta, beta_mac): (RingT, RingT),
-    ) -> Result<((RingT, RingT),(RingT, RingT),(RingT, RingT)), Error> {
+    ) -> Result<((RingT, RingT), (RingT, RingT), (RingT, RingT)), Error> {
         let z = alpha * beta;
         let (z, z_mac) = self.input(channel, z)?;
 
         Ok(((alpha, alpha_mac), (beta, beta_mac), (z, z_mac)))
     }
 
-    pub fn check_zero<C: AbstractChannel> () {
+    pub fn check_zero<C: AbstractChannel>() {
         // todo: C is supposed to be straight up public, so we need to subtract A*B from C and check
         //  if it's 0
     }
 
-    pub fn check_multiply<C: AbstractChannel> (
+    pub fn check_multiply<C: AbstractChannel>(
         &mut self,
         channel: &mut C,
-        triples: &mut [((RingT,RingT), (RingT,RingT), (RingT,RingT))],
+        triples: &mut [((RingT, RingT), (RingT, RingT), (RingT, RingT))],
         multi_thread: bool,
         chunk_size: usize,
-    ) -> Result<(), Error>{
-
+    ) -> Result<(), Error> {
         let mut U = RingT::default();
         let mut V = RingT::default();
 
@@ -149,24 +136,33 @@ where
         if multi_thread {
             let t_start = Instant::now();
 
-            let (U_out, V_out) = triples.par_chunks_exact_mut(chunk_size).map(|x| {
-                let mut rng = AesRng::from_seed(Block::default());
-                let (wl_U, wl_V): (RingT, RingT) = x.into_iter().fold((RingT::default(), RingT::default()),
-                                                                      |(mut tmp_U, mut tmp_V), (alpha, beta, gamma)| {
-                                                                          let chi = rng.gen::<RingT>();
-                                                                          let u = ((alpha.1 * beta.1) * chi);
-                                                                          let v = ((beta.0 * alpha.1) + (alpha.0 * beta.1) - gamma.1) * chi;
-                                                                          (tmp_U + u, tmp_V + v)
-                                                                      });
-                (wl_U, wl_V)
-            }).fold(|| (RingT::default(), RingT::default()), |(U, V), (tmp_u, tmp_v)| {
-                (U + tmp_u, V + tmp_v)
-            }).reduce(|| (RingT::default(), RingT::default()),
-                      |(mut tmp_U, mut tmp_V), (alpha, beta)| {
-                          let u = alpha;
-                          let v = beta;
-                          (tmp_U + u, tmp_V + v)
-                      });
+            let (U_out, V_out) = triples
+                .par_chunks_exact_mut(chunk_size)
+                .map(|x| {
+                    let mut rng = AesRng::from_seed(Block::default());
+                    let (wl_U, wl_V): (RingT, RingT) = x.into_iter().fold(
+                        (RingT::default(), RingT::default()),
+                        |(tmp_U, tmp_V), (alpha, beta, gamma)| {
+                            let chi = rng.gen::<RingT>();
+                            let u = (alpha.1 * beta.1) * chi;
+                            let v = ((beta.0 * alpha.1) + (alpha.0 * beta.1) - gamma.1) * chi;
+                            (tmp_U + u, tmp_V + v)
+                        },
+                    );
+                    (wl_U, wl_V)
+                })
+                .fold(
+                    || (RingT::default(), RingT::default()),
+                    |(U, V), (tmp_u, tmp_v)| (U + tmp_u, V + tmp_v),
+                )
+                .reduce(
+                    || (RingT::default(), RingT::default()),
+                    |(tmp_U, tmp_V), (alpha, beta)| {
+                        let u = alpha;
+                        let v = beta;
+                        (tmp_U + u, tmp_V + v)
+                    },
+                );
 
             println!("Time elapsed mul: {}", t_start.elapsed().as_millis());
             U = U_out;
@@ -178,23 +174,22 @@ where
                 let chi: RingT = seeded_rng.gen::<RingT>();
 
                 // 0 is x (w), 1 is z (m)
-                let w_alpha = cur.0.0;
-                let m_alpha = cur.0.1;
+                let w_alpha = cur.0 .0;
+                let m_alpha = cur.0 .1;
 
-                let w_beta = cur.1.0;
-                let m_beta = cur.1.1;
+                let w_beta = cur.1 .0;
+                let m_beta = cur.1 .1;
 
-                let w_gamma = cur.2.0;
-                let m_gamma = cur.2.1;
+                // let w_gamma = cur.2.0;
+                let m_gamma = cur.2 .1;
 
                 let a0i = m_alpha * m_beta;
                 let a1i = (w_beta * m_alpha) + (w_alpha * m_beta) - m_gamma;
 
-                U += (chi * a0i);
-                V += (chi * a1i);
+                U += chi * a0i;
+                V += chi * a1i;
             }
             println!("Time elapsed mul: {}", t_start.elapsed().as_millis());
-
         }
         self.stats.linear_comb_time = check_time.elapsed();
 
@@ -203,12 +198,9 @@ where
         U += A0;
         V += A1;
 
-
-        channel.send(&U);
-        channel.send(&V);
-
+        channel.send(&U)?;
+        channel.send(&V)?;
 
         Ok(())
     }
-
 }
--- ocelot/src/quicksilver/verifier.rs
+++ ocelot/src/quicksilver/verifier.rs
@@ -1,17 +1,15 @@
-use std::sync::mpsc::channel;
-use std::time::{Duration, Instant};
-use rand::{CryptoRng, Rng, SeedableRng};
-use rand::distributions::{Distribution, Standard};
-use scuttlebutt::{AbstractChannel, AesRng, Block};
-use scuttlebutt::channel::{Receivable, Sendable};
-use scuttlebutt::ring::NewRing;
-use crate::Error;
-use crate::ot::mozzarella::{MozzarellaVerifier, MozzarellaVerifierStats};
 use crate::ot::mozzarella::cache::verifier::CachedVerifier;
 use crate::ot::mozzarella::lpn::LLCode;
-use serde::Serialize;
-
+use crate::ot::mozzarella::{MozzarellaVerifier, MozzarellaVerifierStats};
+use crate::Error;
+use rand::distributions::{Distribution, Standard};
+use rand::{CryptoRng, Rng, SeedableRng};
 use rayon::prelude::*;
+use scuttlebutt::channel::{Receivable, Sendable};
+use scuttlebutt::ring::NewRing;
+use scuttlebutt::{AbstractChannel, AesRng, Block};
+use serde::Serialize;
+use std::time::{Duration, Instant};
 
 #[allow(non_snake_case)]
 pub struct Verifier<'a, RingT>
@@ -25,7 +23,6 @@ where
     stats: VerifierStats,
 }
 
-
 #[derive(Copy, Clone, Debug, Default, Serialize)]
 pub struct VerifierStats {
     pub mozz_init: Duration,
@@ -33,24 +30,22 @@ pub struct VerifierStats {
     pub mozzarella_stats: MozzarellaVerifierStats,
 }
 
-
-
 #[allow(non_snake_case)]
-impl <'a, RingT: NewRing> Verifier<'a, RingT>
-    where
-        RingT: NewRing + Receivable,
-        Standard: Distribution<RingT>,
-        for<'b> &'b RingT: Sendable,
+impl<'a, RingT: NewRing> Verifier<'a, RingT>
+where
+    RingT: NewRing + Receivable,
+    Standard: Distribution<RingT>,
+    for<'b> &'b RingT: Sendable,
 {
-    pub fn init<C: AbstractChannel>(delta: &mut RingT,
-                code: &'a LLCode<RingT>,
-                channel: &mut C,
-                cache: CachedVerifier<RingT>,
-                base_vole_len: usize,
-                num_sp_voles: usize,
-                sp_vole_len: usize,
+    pub fn init<C: AbstractChannel>(
+        delta: &mut RingT,
+        code: &'a LLCode<RingT>,
+        channel: &mut C,
+        cache: CachedVerifier<RingT>,
+        base_vole_len: usize,
+        num_sp_voles: usize,
+        sp_vole_len: usize,
     ) -> Self {
-
         let mut mozVerifier = MozzarellaVerifier::<RingT>::new(
             cache,
             &code,
@@ -69,13 +64,11 @@ impl <'a, RingT: NewRing> Verifier<'a, RingT>
 
         // todo: run extend here so we have enough and can count the time required to do so
 
-
-       Self {
-           mozVerifier,
-           delta: *delta,
-           stats,
-
-       }
+        Self {
+            mozVerifier,
+            delta: *delta,
+            stats,
+        }
     }
 
     pub fn get_stats(&mut self) -> VerifierStats {
@@ -89,18 +82,12 @@ impl <'a, RingT: NewRing> Verifier<'a, RingT>
     }
 
     // The mozVerifier already handles if there aren't any left, in which case it runs extend
-    pub fn random<C: AbstractChannel>(
-        &mut self,
-        channel: &mut C,
-    ) -> Result<RingT, Error> {
+    pub fn random<C: AbstractChannel>(&mut self, channel: &mut C) -> Result<RingT, Error> {
         let y = self.mozVerifier.vole(channel)?;
-        return Ok(y)
+        return Ok(y);
     }
 
-    pub fn input<C: AbstractChannel>(
-        &mut self,
-        channel: &mut C,
-    ) -> Result<RingT, Error>{
+    pub fn input<C: AbstractChannel>(&mut self, channel: &mut C) -> Result<RingT, Error> {
         // todo: ehm.
         let r = self.random(channel)?;
         let diff: RingT = channel.receive()?;
@@ -108,11 +95,7 @@ impl <'a, RingT: NewRing> Verifier<'a, RingT>
         Ok(out)
     }
 
-    pub fn add (
-        &mut self,
-        alpha: RingT,
-        beta: RingT,
-    ) -> Result<RingT, Error> {
+    pub fn add(&mut self, alpha: RingT, beta: RingT) -> Result<RingT, Error> {
         Ok(alpha + beta)
     }
 
@@ -125,24 +108,22 @@ impl <'a, RingT: NewRing> Verifier<'a, RingT>
         Ok((alpha, beta, out))
     }
 
-    pub fn check_multiply<C: AbstractChannel, R: CryptoRng + Rng> (
+    pub fn check_multiply<C: AbstractChannel, R: CryptoRng + Rng>(
         &mut self,
         channel: &mut C,
         mut rng: R,
         triples: &mut [(RingT, RingT, RingT)],
         multi_thread: bool,
         chunk_size: usize,
-    ) -> Result<(), Error>{
-
+    ) -> Result<(), Error> {
         let mut W = RingT::default();
 
         let seed = rng.gen::<Block>();
-        channel.send(&seed);
+        channel.send(&seed)?;
         let t_start = Instant::now();
         let mut seeded_rng = AesRng::from_seed(seed);
         println!("Time to create rng: {}", t_start.elapsed().as_millis());
         let check_start = Instant::now();
-
 
         if multi_thread {
             let t_start = Instant::now();
@@ -153,17 +134,18 @@ impl <'a, RingT: NewRing> Verifier<'a, RingT>
             //let mut tmp: Vec<(&RingT, &(RingT, RingT, RingT))> = chis.iter().zip(triples.iter()).collect();
             println!("Zipping: {}", t_start.elapsed().as_millis());
             let t_start = Instant::now();
-            W = triples.par_chunks_exact_mut(chunk_size).enumerate()
-                .map(|(idx, x)| {
-                    // todo: An initial seed should be sent from the verifier prior to this
+            W = triples
+                .par_chunks_exact_mut(chunk_size)
+                .enumerate()
+                .map(|(_idx, x)| {
+                    // TODO: An initial seed should be sent from the verifier prior to this
                     let mut rng = AesRng::from_seed(Block::default());
-                    x.into_iter().map(|y| {
-                        rng.gen::<RingT>() * ((y.0 * y.1) + (y.2 * self.delta))
-                    }).sum()
-                }
-            ).sum();
+                    x.into_iter()
+                        .map(|y| rng.gen::<RingT>() * ((y.0 * y.1) + (y.2 * self.delta)))
+                        .sum()
+                })
+                .sum();
             println!("Computing sum: {}", t_start.elapsed().as_millis());
-
         } else {
             let t_start = Instant::now();
             for (x, y, z) in triples.iter() {
@@ -171,22 +153,19 @@ impl <'a, RingT: NewRing> Verifier<'a, RingT>
 
                 let bi = (*x) * (*y) + (*z * self.delta);
 
-                W += (bi * chi);
+                W += bi * chi;
             }
             println!("Computing sum: {}", t_start.elapsed().as_millis());
-
         }
         let B = self.random(channel)?;
         W += B;
 
         self.stats.linear_comb_time = check_start.elapsed();
 
-
         let U: RingT = channel.receive()?;
         let V: RingT = channel.receive()?;
 
-        let tmp = (U - (V * self.delta));
-
+        let tmp = U - (V * self.delta);
 
         if W == tmp {
             println!("Check passed");
@@ -196,5 +175,4 @@ impl <'a, RingT: NewRing> Verifier<'a, RingT>
             Err(Error::Other("checkMultiply fails".to_string()))
         }
     }
-
 }
